f10385664cab52e6a04e95fc8ccda9d779b23468
thozhatech00 <sathishsrm94@gmail.com>
2026-06-11 14:13:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+const menuToggle=document.getElementById('menuToggle');const siteNav=document.getElementById('siteNav');if(menuToggle&&siteNav){menuToggle.addEventListener('click',()=>siteNav.classList.toggle('open'));siteNav.querySelectorAll('a').forEach(link=>link.addEventListener('click',()=>siteNav.classList.remove('open')));}
