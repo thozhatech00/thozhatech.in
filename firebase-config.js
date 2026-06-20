433a9680a4113c19c1e5f12d970e9f8e5eda232f
thozhatech00 <sathishsrm94@gmail.com>
2026-06-20 23:43:17 +0530

    done

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBgYDrXphiTg4lY65sdeUP58uFs6R3sS9M",
-  authDomain: "thozhatech-portal.firebaseapp.com",
-  projectId: "thozhatech-portal",
-  storageBucket: "thozhatech-portal.firebasestorage.app",
-  messagingSenderId: "413436933035",
-  appId: "1:413436933035:web:70abff48fe463f981b98ee"
-};
